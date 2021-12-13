8a42f19567350e772d56ec9d0df115062fad4080
Bennie Rosas <ben@aliencyb.org>
2021-12-13 12:31:07 -0600

    install and configure tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :knight_moves, KnightMovesWeb.Endpoint,
   secret_key_base: "GgzrzGAwfCBZa835CA4gW/Xu8fbYfuctJyVlK7VC8zMggudAAzviO0rPOv3bcZI3",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
