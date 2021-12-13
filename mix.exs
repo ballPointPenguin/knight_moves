db9c0909d90c90571d7af32b987c7b9c24cf12cd
Bennie Rosas <ben@aliencyb.org>
2021-12-13 12:31:07 -0600

    install binbo library

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule KnightMoves.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # Included with Phoenix
       {:phoenix, "~> 1.6.4"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
@@ -48,7 +49,10 @@ defmodule KnightMoves.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      # Added by me
+      {:binbo, "~> 3.0"}
     ]
   end
 
