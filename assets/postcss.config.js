143ca97bafc6b2bb6a050ff6cc88f40b8c4c7c0f
Bennie Rosas <ben@aliencyb.org>
2021-12-13 22:38:35 -0600

    use cssnano to minify css

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -3,5 +3,7 @@ module.exports = {
     "postcss-import": {},
     tailwindcss: {},
     autoprefixer: {},
+    // Use cssnano to minify code in production
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   }
 }
